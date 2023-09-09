9cd737ea2f041976d55ecf927d56a7d16b90e95c
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-09-09 14:01:54 -0500

    adding client

---
+++ go-gsm-protocol/main.go
@@ -0,0 +1,17 @@
+package main
+
+// Client represents a UCP client connection.
+type Client struct {
+	// IP:PORT address of the SMSC
+	addr string
+	// SMSC username
+	user string
+	// SMSC pasword
+	password string
+	// SMSC accesscode
+	accessCode string
+}
+
+func main() {
+
+}
