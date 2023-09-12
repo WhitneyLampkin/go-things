25f35bfbe5eb58a236efc92925bb7bc7c4bc1b13
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-09-12 05:50:37 -0500

    adding original README.md, link and MIT license

--- go-cli-tool/csv2json.go
+++ go-cli-tool/csv2json.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // Struct that stores the user inputted file information needed
@@ -135,7 +137,7 @@ func check(e error) {
 func processLine(headers []string, dataList []string) (map[string]string, error) {
 	// Validating if we're getting the same number of headers and columns. Otherwise, we return an error
 	if len(dataList) != len(headers) {
-		return nil, errors.New("Line doesn't match headers format. Skipping")
+		return nil, errors.New("line doesn't match headers format (skipping)")
 	}
 	// Creating the map we're going to populate
 	recordMap := make(map[string]string)
@@ -147,9 +149,102 @@ func processLine(headers []string, dataList []string) (map[string]string, error)
 	return recordMap, nil
 }
 
-func main() {
-	// Manual test
-	fileData, err := getFileData()
+func writeJSONFile(csvPath string, writerChannel <-chan map[string]string, done chan<- bool, pretty bool) {
+	writeString := createStringWriter(csvPath) // Instanciating a JSON writer function
+	jsonFunc, breakLine := getJSONFunc(pretty) // Instanciating the JSON parse function and the breakline character
+	// Log for informing
+	fmt.Println("Writing JSON file...")
+	// Writing the first character of our JSON file. We always start with a "[" since we always generate array of record
+	writeString("["+breakLine, false)
+	first := true
+	for {
+		// Waiting for pushed records into our writerChannel
+		record, more := <-writerChannel
+		if more {
+			if !first { // If it's not the first record, we break the line
+				writeString(","+breakLine, false)
+			} else {
+				first = false // If it's the first one, we don't break the line
+			}
 
-	fmt.Println(fileData, err)
+			jsonData := jsonFunc(record) // Parsing the record into JSON
+			writeString(jsonData, false) // Writing the JSON string with our writer function
+		} else { // If we get here, it means there aren't more record to parse. So we need to close the file
+			writeString(breakLine+"]", true) // Writing the final character and closing the file
+			fmt.Println("Completed!")        // Logging that we're done
+			fmt.Println("The new json file has been added to the current directory with the same name as the one provided.")
+			done <- true // Sending the signal to the main function so it can correctly exit out.
+			break        // Stoping the for-loop
+		}
+	}
+}
+
+func createStringWriter(csvPath string) func(string, bool) {
+	jsonDir := filepath.Dir(csvPath)                                                       // Getting the directory where the CSV file is
+	jsonName := fmt.Sprintf("%s.json", strings.TrimSuffix(filepath.Base(csvPath), ".csv")) // Declaring the JSON filename, using the CSV file name as base
+	finalLocation := filepath.Join(jsonDir, jsonName)                                      // Declaring the JSON file location, using the previous variables as base
+	// Opening the JSON file that we want to start writing
+	f, err := os.Create(finalLocation)
+	check(err)
+	// This is the function we want to return, we're going to use it to write the JSON file
+	return func(data string, close bool) { // 2 arguments: The piece of text we want to write, and whether or not we should close the file
+		_, err := f.WriteString(data) // Writing the data string into the file
+		check(err)
+		// If close is "true", it means there are no more data left to be written, so we close the file
+		if close {
+			f.Close()
+		}
+	}
+}
+
+func getJSONFunc(pretty bool) (func(map[string]string) string, string) {
+	// Declaring the variables we're going to return at the end
+	var jsonFunc func(map[string]string) string
+	var breakLine string
+	if pretty { //Pretty is enabled, so we should return a well-formatted JSON file (multi-line)
+		breakLine = "\n"
+		jsonFunc = func(record map[string]string) string {
+			jsonData, _ := json.MarshalIndent(record, "   ", "   ") // By doing this we're ensuring the JSON generated is indented and multi-line
+			return "   " + string(jsonData)                         // Transforming from binary data to string and adding the indent characets to the front
+		}
+	} else { // Now pretty is disabled so we should return a compact JSON file (one single line)
+		breakLine = "" // It's an empty string because we never break lines when adding a new JSON object
+		jsonFunc = func(record map[string]string) string {
+			jsonData, _ := json.Marshal(record) // Now we're using the standard Marshal function, which generates JSON without formating
+			return string(jsonData)             // Transforming from binary data to string
+		}
+	}
+
+	return jsonFunc, breakLine // Returning everythinbg
+}
+
+func main() {
+	// Provide help info when users enter the --help option using the anonymous function below
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options] <csvFile>\nOptions:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	// Get file data entered by user
+	fileData, err := getFileData()
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	// Validate the file provided if no errors
+	if _, err := checkIfValidFile(fileData.filepath); err != nil {
+		exitGracefully(err)
+	}
+
+	// Declare channels for the go-routines to use
+	writerChannel := make(chan map[string]string)
+	done := make(chan bool)
+
+	// Starting the go-routines used for reading and writing
+	// These two will run asynchronously
+	go processCsvFile(fileData, writerChannel)
+	go writeJSONFile(fileData.filepath, writerChannel, done, fileData.pretty)
+
+	// Terminate program execution when done
+	<-done
 }
